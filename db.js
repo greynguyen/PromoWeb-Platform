21ffd09d2028abc4917287532b90caf4d102bf3f
Xuerui <xuerui@hntvlan569.0702.wlan.wireless-resnet.upenn.edu>
2017-03-16 15:45:07 -0400

    Updating from laptop

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
-var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://Snap2Snap:Snap2Snap@ds147799.mlab.com:47799/snap2snap', function (err) {
+	if (err) {
+		console.log('Error connecting to mongodb database: %s.\nIs "mongod" running?', err.message);
+		process.exit(0);
+	}
+});
+
+//We're connected!
+
+var db = mongoose.connection;
+
+var addProfile = function (user, pass, email, callback) {
+	
+}
